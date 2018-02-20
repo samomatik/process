e11dff81a728bf4745cc146390d37fadc62c90b2
Samitha Padukkage Don <sam@samomtik.com>
2018-02-21 07:44:36 +1100

    Added arrangement fields and a text field to simplify stripping numbers

--- scripts.js
+++ scripts.js
@@ -6,5 +6,22 @@ $(document).ready(function() {
   $(".clear").click(function() {
     $(".select").removeClass("done");
     $("textarea").val('');
+    $("#before").val('');
+    $(".arrgField").val('');
   });
 });
+
+function replacer() {
+
+  var str = document.getElementById('before').value.
+    replace(/\(.+?\)/g, '').
+    replace(/[^a-z0-9+]+/gi, '');
+
+  document.getElementById('before').value = str;
+
+  var copyText = document.getElementById("before");
+  copyText.select();
+  document.execCommand("Copy");
+}
+
+document.getElementById('replacem').onclick = replacer;
